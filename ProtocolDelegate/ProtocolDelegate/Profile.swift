8ddebe6626d576a3b6e69198e42fc6abe41a864a
Riandro Proenca <73947142+RiandroProenca@users.noreply.github.com>
2021-11-16 14:45:04 -0300

    Add files via upload

---
+++ ProtocolDelegate/ProtocolDelegate/Profile.swift
@@ -0,0 +1,15 @@
+//
+//  Profile.swift
+//  ProtocolDelegate
+//
+//  Created by Riandro Proença on 16/11/21.
+//
+
+import UIKit
+
+struct Profile {
+    let icon: String
+    let accentColor: UIColor
+    let name: String
+    let info: String
+}
